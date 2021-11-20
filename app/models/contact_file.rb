7137d6707864defeaef1e23fc819381ebf37359a
Sebastian Viloria R <seviloria@uninorte.edu.co>
2021-11-19 21:15:02 -0500

    Create contact without any validation

--- app/models/contact_file.rb
+++ app/models/contact_file.rb
@@ -1,4 +1,14 @@
 class ContactFile < ApplicationRecord
-  has_one_attached :csv
   belongs_to :user
+
+  after_create_commit :generate_contacts
+  
+  has_one_attached :csv
+
+  private
+  
+  def generate_contacts
+    binding.pry
+    ContactCreator.new(id).call
+  end
 end
