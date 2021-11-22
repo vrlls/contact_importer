01aebc324bc3a2b11d370bf19f0fffdfa8f5d091
Sebastian Viloria <seviloria@uninorte.edu.co>
2021-11-22 09:33:21 -0500

    sidekiq route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   resources :contact_files, except: [:update, :destroy, :edit]
   devise_for :users
   root 'contacts#index'
+
+  if defined?(Sidekiq) && defined?(Sidekiq::Web)
+    mount Sidekiq::Web => '/sidekiq'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
